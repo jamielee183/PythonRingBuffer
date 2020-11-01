e6c669f4956e91de00144cc85553970aa9fe0515
jamielee183 <60348484+jamielee183@users.noreply.github.com>
2020-11-01 18:33:38 +0000

    More pythonic,

--- ringBuffer.py
+++ ringBuffer.py
@@ -8,16 +8,19 @@ class RingBuffer:
         self._data = np.zeros(self.bufferLength)
         self.pointer = 0
         self.fullFlag = False
-        self.get = lambda : self.getFull() if  self.fullFlag else np.roll(self.getNotFull(),self.bufferLength-self.pointer)
-        self.append = lambda x : self.appendFull(x) if self.fullFlag else self.appendNotFull(x)
-
         
+        self.getNotFull = lambda : self._data
+        self.getFull = lambda : np.append(self._data[self.pointer:], self._data[:self.pointer])
+        self.getBuff = lambda : self.getFull() if  self.fullFlag else np.roll(self.getNotFull(),self.bufferLength-self.pointer)
+
+        self.get = lambda : np.flip(self.getBuff(),0)
+        self.append = lambda x : self.appendFull(x) if self.fullFlag else self.appendNotFull(x)
 
     def appendNotFull(self, sample):
         # add an element at the pointer location in the buffer
         self._data[self.pointer] = sample
         self.pointer += 1
-        
+     
         if self.pointer == self.bufferLength:
             self.pointer = 0    
             self.fullFlag = True    
@@ -27,13 +30,6 @@ class RingBuffer:
         self._data[self.pointer] = sample
         self.pointer = (self.pointer+1) % self.bufferLength
 
-    def getNotFull(self):
-        return self._data
-
-    def getFull(self):
-        # correct the order of the _data
-        return np.append(self._data[self.pointer:], self._data[:self.pointer])
-
     @property
     def data(self):
         return self._data
@@ -42,23 +38,23 @@ class RingBuffer:
 # sample usage
 if __name__=='__main__':
     x=RingBuffer(5)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(1)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(2)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(3)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(4)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(5)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(6)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(7)
     x.append(8)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
     x.append(9)
     x.append(10)
     x.append(11)
-    print(x.data, x.get(), np.flip(x.get(),0))
+    print(x.data, x.get())
