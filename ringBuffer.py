ead0d5217f7f82dcaca6cbadf10f8eb52a0c0b4f
jamielee183 <60348484+jamielee183@users.noreply.github.com>
2020-11-06 10:52:14 +0000

    concat anf flipud faster

--- ringBuffer.py
+++ ringBuffer.py
@@ -1,18 +1,13 @@
 import numpy as np
 
-
-
-
-
 class RingBuffer:
 
     def __init__(self, bufferLength):
         self.bufferLength = bufferLength
         self._data = np.zeros(self.bufferLength)
         self.pointer = 0
-        self.getNotFull = lambda : self._data
-        self.getBuff = lambda : np.append(self._data[self.pointer:], self._data[:self.pointer])
-        self.get = lambda : np.flip(self.getBuff(),0)
+        self.getBuff = lambda : np.concatenate((self._data[self.pointer:], self._data[:self.pointer]))
+        self.get = lambda : np.flipud(self.getBuff())
 
 
     def append(self, sample):
@@ -27,23 +22,23 @@ class RingBuffer:
 # sample usage
 if __name__=='__main__':
     x=RingBuffer(5)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(1)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(2)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(3)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(4)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(5)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(6)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(7)
     x.append(8)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
     x.append(9)
     x.append(10)
     x.append(11)
-    print(x.data, x.get())
+    print(x.data, x.getBuff(), x.get())
